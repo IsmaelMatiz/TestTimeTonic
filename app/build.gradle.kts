e504643ff530d29faf5e7659ea62a876944ec089
IsmaelMatiz <ismaelmatiz@gmail.com>
2024-03-11 20:20:06 -0500

    Handling images
    the coil dependency has been added for bring
    images from the web, required more ahead in
    the app ic_launcher has been updated and added
    to the login screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.9.0")
--- app/src/main/java/com/example/testtimetonic/ui/login/LoginView.kt
+++ app/src/main/java/com/example/testtimetonic/ui/login/LoginView.kt
@@ -1,9 +1,48 @@
 package com.example.testtimetonic.ui.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.testtimetonic.R
 
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun LoginView(){
-    Text(text = "Login View")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+            Image(painter = painterResource(id = R.drawable.book_store),
+                contentDescription = "appLogo",
+                modifier = Modifier.size(360.dp))
+            Spacer(modifier = Modifier.padding(16.dp))
+            //EmailField(email,{viewModel.onLoginChanged(it, password)})
+            Spacer(modifier = Modifier.padding(16.dp))
+            //PasswordField(password, {viewModel.onLoginChanged(email, it)})
+            Spacer(modifier = Modifier.padding(16.dp))
+            //ForgotPassword(modifier = Modifier.align(alignment = Alignment.End))
+            Spacer(modifier = Modifier.padding(16.dp))
+            /*LoginButton(loginEnable = loginEnable) {
+                coroutineScope.launch {
+                    viewModel.onLoginSelected()
+                }
+
+             */
+        }
 }
+
