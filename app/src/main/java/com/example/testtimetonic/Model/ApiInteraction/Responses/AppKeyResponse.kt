64d66101a4082db9ddc08e2487da86a89a3a0b5d
IsmaelMatiz <ismaelmatiz@gmail.com>
2024-03-13 10:57:24 -0500

    forgot | documentation added to AppKeyResponse

--- app/src/main/java/com/example/testtimetonic/Model/ApiInteraction/Responses/AppKeyResponse.kt
+++ app/src/main/java/com/example/testtimetonic/Model/ApiInteraction/Responses/AppKeyResponse.kt
@@ -7,10 +7,21 @@ import com.example.testtimetonic.Model.Crythographer.KeyName
 import com.example.testtimetonic.Model.Crythographer.getSecretDocument
 import java.io.FileOutputStream
 
+/**
+ * This class represents a response object containing information retrieved from the app key API.
+ *
+ * @property context The application context (typically passed from the activity or fragment).
+ * @property status The status of the app key retrieval request ("ok" or "nok").
+ * @property appkey The encrypted app key (if successful) or "null" otherwise.
+ */
 class AppKeyResponse(var context: Context){
     var status: String = ""
+    /**
+     * The app key retrieved from the API. This is encrypted and store in a file
+     */
     var appkey: String = ""
         set(value) {
+            // Encrypt the app key if it's not null
             if (value != Constants.RETURN_NULL.constanVal){
                 val file = getSecretDocument(KeyName.SECRET_KEY_APPKEY,context)
                 val fos = FileOutputStream(file)
@@ -20,9 +31,10 @@ class AppKeyResponse(var context: Context){
                     bytes = value.encodeToByteArray(),
                     outputStream = fos,
                     KeyName.SECRET_KEY_APPKEY
-                )?.decodeToString().toString()
+                )?.decodeToString().toString()// Handle null case
 
             }else{
+                // Set appkey to "null"
                 field = Constants.RETURN_NULL.constanVal
             }
         }
